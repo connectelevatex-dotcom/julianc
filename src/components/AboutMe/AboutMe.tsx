56942c927964a0d1a52c18051d7270ab642f7de9
connectelevatex-dotcom <connect.elevatex@gmail.com>
2025-08-24 14:44:17 +0500

    Fix JSX Syntax Error in AboutMe Component

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -17,6 +17,7 @@ const AboutMe: React.FC = () => {
               />
             </div>
           </AnimatedSection
+          </AnimatedSection>
 
           {/* Right Section: Text Content */}
           <div className="w-full lg:w-1/2 text-center lg:text-left px-4 sm:px-0">
